d9d0afe3c012d018f24721de3a58a00d241e65a7
im612 <imereu.cnio@gmail.com>
2023-04-28 12:13:26 +0200

    lancia uvicorn e la lista appare? strano che non trova sklearn

--- backend/model/model.py
+++ backend/model/model.py
@@ -10,7 +10,8 @@ from pathlib import Path
 # import streamlit as st
 # from pydantic import BaseModel
 from itertools import chain
-import sklearn
+# import sklearn
+import scikit-learn
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
